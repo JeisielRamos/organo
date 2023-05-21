28fb8d89319038304f64920527a12e263a145f1a
jeisiel.oliveira <jeisielramos@gmail.com>
2023-05-21 18:06:04 -0300

    init desing team

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
 
@@ -14,6 +15,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form registeredWorker = { worker => SetNewWorker(worker)}/>
+      <Team name="Liderança"/>
+      <Team name="Geografia"/>
+      <Team name="Novo Testamento"/>
      </div>
   );
 }
