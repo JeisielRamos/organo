0736251d9ee023f1b000f8975d70fc1db719f48f
jeisiel.oliveira <jeisielramos@gmail.com>
2023-05-21 16:59:45 -0300

    knowing value change in react

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,12 +1,17 @@
 import './InputText.css'
 
 export const InputText = (props) => {
+
+    const whenTyping = (event) => {
+        var valor = event.target.value
+        console.log(valor);
+    }
     return (
         <div className="input-text">
             <label> 
                 {props.label} 
             </label>
-            <input required={props.required} placeholder={props.placeholder}/>
+            <input onChange={whenTyping} required={props.required} placeholder={props.placeholder}/>
         </div>
     )
 }
