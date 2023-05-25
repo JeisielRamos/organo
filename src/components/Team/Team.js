d0b561de631ebb62a99b0e93fc94752acbe66294
jeisiel.oliveira <jeisielramos@gmail.com>
2023-05-24 22:54:17 -0300

    worker card

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,3 +1,4 @@
+import Worker from '../Worker'
 import './Team.css'
 
 export const Team = (props) => {
@@ -5,6 +6,7 @@ export const Team = (props) => {
     return (
         <section className='team' style={styleSection} >
             <h3 style={{borderColor: props.primaryColor}}>{props.name}</h3>
+            <Worker />
         </section>
     )
 }
---
+++ src/components/Worker/Worker.js
@@ -0,0 +1,15 @@
+import './Worker.css'
+
+export const Worker = () => {
+    return (
+        <div className='worker'>
+            <div className='header'>
+                <img src='https://github.com/JeisielRamos.png' alt='Jeisiel'/>
+            </div>
+            <div className='footer'>
+                <h4>Jeisiel</h4>
+                <h5>Dev</h5>
+            </div>
+        </div>
+    )
+}
